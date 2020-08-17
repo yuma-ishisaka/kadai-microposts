7d510f5d62a9dda62b711c6b03454dd531cf9040
yuma-ishisaka <iy.opb.smlh@gmail.com>
2020-08-17 07:09:09 +0000

    post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
-  before_save { self.email.downcase! } #save前にemailを小文字化
-  validates :name, presence: true, length: { maximum: 50 } #文字数制限
-  validates :email, presence: true, length: { maximum: 255 }, #文字数制限
-                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, #メアド形式の指定
-                    uniqueness: { case_sensitive: false } #重複の禁止
+  before_save { self.email.downcase! }
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
   has_secure_password
+
+  has_many :microposts
 end
