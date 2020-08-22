ce9c4482a90cff15202bccb58a1ae1c44966e60f
yuma-ishisaka <iy.opb.smlh@gmail.com>
2020-08-22 08:29:58 +0000

    follow, unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+
   def index
-    @users = User.order(id: :desc).page(params[:page]).per(25) #id降順取得
+    @users = User.order(id: :desc).page(params[:page]).per(25)
   end
 
   def show
@@ -15,15 +16,27 @@ class UsersController < ApplicationController
   end
 
   def create
-     @user = User.new(user_params)
+    @user = User.new(user_params)
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user #処理後にuser#showに移動
+      redirect_to @user
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
-      render :new　#users/new.html.newに移動
+      render :new
     end
+  end
+
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.page(params[:page])
+    counts(@user)
+  end
+  
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.page(params[:page])
+    counts(@user)
   end
 
   private
